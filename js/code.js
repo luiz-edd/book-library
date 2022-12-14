0d2953a80043b1b818416e11f2b07233d6125eb5
Luiz Eduardo <eduardol@fastmail.com>
2022-12-14 13:10:07 -0300

    add remove book function to remove the especify book from the myLibrary array

--- js/code.js
+++ js/code.js
@@ -5,7 +5,7 @@ const form = document.querySelector("#form-book");
 const title = form.elements["title"];
 const author = form.elements["author"];
 const pages = form.elements["pages"];
-const readed = form.elements["readed"];
+const read = form.elements["read"];
 let btnRemove = document.querySelectorAll(".btn-remove");
 
 
@@ -16,18 +16,19 @@ let btnRemove = document.querySelectorAll(".btn-remove");
 // <div>Author: <span></span></div>
 // <div>Pages: <span></span></div>
 // <div>ID: <span></span></div>
-// <div>Readed: <span></span></div>`;
+// <div>read: <span></span></div>`;
 
 // constructor and prototypes
-function Book(title, author, pages, readed) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.readed = readed;
+    this.read = read;
+    this.id = Math.floor(Math.random() * 100000);
     addBookToLibrary(this);
 }
 Book.prototype.info = function () {
-    return `Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}, Readed: ${this.readed}`;
+    return `Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}, read: ${this.read}`;
 }
 
 //functions
@@ -37,44 +38,53 @@ function addBookToLibrary(book) {
 function displayLibrary() {
     let i = 0;
     myLibrary.forEach(book => {
-        console.log("Book number " + i + ": " + book.info() + "\n----------------- ");
+        console.log("Book number " + i + ": " + book.info());
         i++;
     });
+    console.log("------------------------------------- ")
 }
-function addCardBookText(cardNode) {
+function addCardBookText(cardNode, newBook) {
     // let cardBook = document.createElement('div');
     let auxString = ""
-    readed.checked ? auxString = "Yes" : auxString = "No";
+    read.checked ? auxString = "Yes" : auxString = "No";
     cardNode.innerHTML = `<div>Title: <span>${title.value}</span></div>
     <div>Author: <span>${author.value}</span></div>
     <div>Pages: <span>${pages.value}</span></div>
-    <div>ID: <span></span></div>
-    <div>Readed: <span>${auxString}</span></div>
+    <div>Read: <span>${auxString}</span></div>
     <button class="btn-remove">Remove</button>`;
+    // <div>ID: <span class=">${newBook.id} </span></div>
+
 }
 function createCardBook() {
-    const newBook = new Book(title.value, author.value, pages.value, readed.checked); //create book obj and add to the library array
+    const newBook = new Book(title.value, author.value, pages.value, read.checked); //create book obj and add to the library array
     let bookCard = document.createElement("div");
+    
     bookCard.classList.add("book-card");
-    bookCard.setAttribute("data-index", 1);
-    addCardBookText(bookCard); //take the form value and put into this card 
+    bookCard.setAttribute("data-index", newBook.id);
+    addCardBookText(bookCard, newBook); //take the form value and put into this card
     library.appendChild(bookCard);
-    updateList();
+    removeBookAndUpdateList();
 }
 
 function removeBook(element) {
-    myLibrary.includes()
+    let bookId = element.parentElement.getAttribute("data-index");
+    myLibrary.forEach((element, index, array) => {
+        if(element.id == bookId){
+            myLibrary.splice(index, 1);
+            displayLibrary();
+        }
+    });
     element.parentElement.remove();
 }
 
-// events   
+// events
 form.addEventListener("submit", (event) => {
     event.preventDefault()
     createCardBook();
     form.reset();
 })
 
-function updateList() {
+function removeBookAndUpdateList() {
     btnRemove = document.querySelectorAll(".btn-remove");
     Array.from(btnRemove).forEach(function (element) {
         element.addEventListener('click', (event) => {
